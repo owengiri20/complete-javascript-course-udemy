3b508dc10ffcd3df4d89f35551b76705647f1f82
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-06-18 09:08:40 +0800

    format fractional implemented

--- Forkify/src/js/models/Recipe.js
+++ Forkify/src/js/models/Recipe.js
@@ -57,6 +57,8 @@ export default class Recipe {
       "pound"
     ];
 
+    const units = [...unitsShort, "kg", "g"];
+
     const newIngredients = this.ingredients.map(el => {
       // 1. Uniform units
       let ingredient = el.toLowerCase();
@@ -69,7 +71,7 @@ export default class Recipe {
 
       // 3. Parse ingredienrs into count, unit and ingredient
       const arrIng = ingredient.split(" ");
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
